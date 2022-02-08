5882bd0eac8acb77d2ec1996a5c1cca60d1bb90e
VardanianA <anivardanian99@gmail.com>
2022-02-08 16:07:18 +0400

    added files example

---
+++ users/src/models/users/router.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { createData, deleteData, getData, updateData, createDataGet, updateDataGet, deleteDataGet, middleware } from './controller.js';
+
+const router = express.Router();
+
+router.use(middleware);
+
+//get data
+router.get('/', getData);
+
+//create data
+router.get('/create', createDataGet);
+router.post('/create', createData);
+
+//update data
+router.get('/update', updateDataGet);
+router.post('/update', updateData);
+
+//delete data
+router.get('/delete/:name', deleteDataGet);
+router.delete('/delete/:name', deleteData);
+
+
+export default router;
